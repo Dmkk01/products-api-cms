ccbc7dbd02123845fc7cdd17b055eea290a77ea1
Dominik Łasiński <76247450+Dmkk01@users.noreply.github.com>
2021-11-18 15:29:07 +0200

    Upload

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '41950dac5b83082e2f5104f68430afdc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'de72e480-931c-453d-9cdf-fcad2330e510'
+};
